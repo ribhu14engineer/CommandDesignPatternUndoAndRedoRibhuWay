93807fcb1af082c277f94d969e9fe95919c5d52c
Ribhu Sengupta <ribhuswatgov@gmail.com>
2024-10-09 00:45:36 +0530

    Add project files.

---
+++ TVReceiver/Tv.cs
@@ -0,0 +1,14 @@
+﻿namespace TVReceiver
+{
+    public class TV
+    {
+        bool isOn;
+        int setVolume;
+
+        public void turnOnTv()
+        {
+            isOn = true;
+            Console.WriteLine("Turn On TV");
+        }
+    }
+}
